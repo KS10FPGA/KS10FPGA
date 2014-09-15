06902af79ace9855ad57849c04d27ed02e789a27
Rob Doyle <doyle@cox.net>
2014-09-15 03:59:05 +0000

    *** empty log message ***

--- fpga/ks10/debug/__debug.v
+++ fpga/ks10/debug/__debug.v
@@ -1,13 +1,13 @@
-////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 //
 // KS-10 Processor
 //
 // Brief
-//   Debug Hack
+//   Debug Interface
 //
 // Details
-//
-// Todo
+//   This file is used by the simulator to print the program counter on the
+//   console display as the code executes
 //
 // File
 //   debug.v
@@ -15,39 +15,39 @@
 // Author
 //   Rob Doyle - doyle (at) cox (dot) net
 //
-////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 //
-//  Copyright (C) 2012 Rob Doyle
+//  Copyright (C) 2012-2014 Rob Doyle
 //
-// This source file may be used and distributed without
-// restriction provided that this copyright statement is not
-// removed from the file and that any derivative work contains
-// the original copyright notice and the associated disclaimer.
+// This source file may be used and distributed without restriction provided
+// that this copyright statement is not removed from the file and that any
+// derivative work contains the original copyright notice and the associated
+// disclaimer.
 //
-// This source file is free software; you can redistribute it
-// and/or modify it under the terms of the GNU Lesser General
-// Public License as published by the Free Software Foundation;
-// version 2.1 of the License.
+// This source file is free software; you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published by the
+// Free Software Foundation; version 2.1 of the License.
 //
-// This source is distributed in the hope that it will be
-// useful, but WITHOUT ANY WARRANTY; without even the implied
-// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-// PURPOSE. See the GNU Lesser General Public License for more
-// details.
+// This source is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+// for more details.
 //
-// You should have received a copy of the GNU Lesser General
-// Public License along with this source; if not, download it
-// from http://www.gnu.org/licenses/lgpl.txt
+// You should have received a copy of the GNU Lesser General Public License
+// along with this source; if not, download it from
+// http://www.gnu.org/licenses/lgpl.txt
 //
-////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 
 `default_nettype none
 `include "useq/crom.vh"
 
+//`define DEBUG_DSDZA
+  
   module DEBUG(clk, rst, clken, crom, debugDATA, debugADDR);
-   
+
    parameter cromWidth = `CROM_WIDTH;
-   
+
    input                  clk;          // Clock
    input                  rst;          // Reset
    input                  clken;        // Clock Enable
@@ -58,45 +58,60 @@
    //
    // Microcode Decode
    //
-   
+
 `ifndef SYNTHESIS
-   
+
    wire loadIR = `cromSPEC_EN_40 & (`cromSPEC_SEL == `cromSPEC_SEL_LOADIR);
 
    //
    // Print the Program Counter
    //
    // Details:
-   //  The program counter is modified by instructions in various
-   //  stages of the microcode.  A modified PC does not mean that
-   //  the instruction was actually executed.  For example, the
-   //  PC is always incremented after the instruction is fetched
-   //  but the PC may be modified (again) later by a branch
-   //  instruction.
+   //  The program counter is modified by instructions in various stages of the
+   //  microcode.  A modified PC does not mean that the instruction was
+   //  actually executed.  For example, the PC is always incremented after the
+   //  instruction is fetched but the PC may be modified (again) later by a
+   //  branch instruction.
    //
-   //  This code prints the program counter when the instruction
-   //  register is loaded.  This occurs once during the instruction
-   //  execution.
+   //  This code prints the program counter when the instruction register is
+   //  loaded.  This occurs once during the instruction execution.
    //
 
    reg [0:35] PC;
-   
+   reg [0: 5] test;
+
    always @(posedge clk or posedge rst)
      begin
         if (rst)
-          PC <= 18'b0;
+          begin
+             PC   <= 0;
+             test <= 0;
+          end
         else if (loadIR)
           begin
              PC <= debugDATA;
-             $display("debug: [%.3f]: PC is %06o", $time/1.0e3, debugDATA[18:35]);
+	     if (PC[18:35] ==  18'o030057)
+	         $stop;
+
+             `ifdef DEBUG_DSKCG
+                `include "debug_dskcg.vh"
+             `elsif DEBUG_DSDZA
+                 `include "debug_dsdza.vh"
+             `elsif DEBUG_DSUBA
+                 `include "debug_dsuba.vh"
+             `else
+                  $display("[%10.3f] debug: PC is %06o", $time/1.0e3,
+			   debugDATA[18:35]);
+             `endif
           end
-     end
-`endif
+     end // always @ (posedge clk or posedge rst)
    
+`endif
+
    //
    // PC is Register #1 in the ALU
    //
-   
+
    assign debugADDR = 4'b0001;
-   
+
  endmodule
