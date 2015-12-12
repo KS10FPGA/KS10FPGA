8b6d61bd585d7763401dbbbed2907f80e41df9e0
Rob Doyle <doyle@cox.net>
2015-12-12 06:04:50 +0000

    *** empty log message ***

--- fpga/ks10/mem/mem32k.v
+++ fpga/ks10/mem/mem32k.v
@@ -62,7 +62,7 @@ module MEM (
       output wire         ssramOE_N,    // SSRAM OE#
       output wire         ssramWE_N,    // SSRAM WE#
       output wire         ssramCE,      // SSRAM CE
-      output wire [ 0:22] ssramADDR,    // SSRAM Address Bus
+      output wire [ 0:19] ssramADDR,    // SSRAM Address Bus
       inout  wire [ 0:35] ssramDATA,    // SSRAM Data Bus
       output wire [ 0:27] test          // Test signals
    );
