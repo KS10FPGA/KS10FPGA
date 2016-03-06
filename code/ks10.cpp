4eec04f5f0726e898ef03e1948fc1fcfa1132506
jfcl <radioengr@gmail.com>
2016-03-06 10:58:45 -0700

    Fix file headers.   No code changes

--- code/ks10.cpp
+++ code/ks10.cpp
@@ -19,6 +19,8 @@
 //
 // Copyright (C) 2013-2016 Rob Doyle
 //
+// This file is part of the KS10 FPGA Project
+//
 // The KS10 FPGA project is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by the Free
 // Software Foundation, either version 3 of the License, or (at your option) any
