0f484e0f57563099ada8c2d49b37bc40064a3151
jfcl <doyle@cox.net>
2022-01-07 04:24:10 -0700

    Fix copyright info on MT files.  No code changes.

--- fpga/ks10/mt/mtctrl.sv
+++ fpga/ks10/mt/mtctrl.sv
@@ -13,7 +13,7 @@
 //
 ////////////////////////////////////////////////////////////////////////////////
 //
-// Copyright (C) 2012-2022 Rob Doyle
+// Copyright (C) 2021-2022 Rob Doyle
 //
 // This source file may be used and distributed without restriction provided
 // that this copyright statement is not removed from the file and that any
